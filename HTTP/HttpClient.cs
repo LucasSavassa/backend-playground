777c03a494452d563c3b128ea745493c3e0b5269
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-11-13 13:37:18 -0300

    Add HTTP project

---
+++ HTTP/HttpClient.cs
@@ -0,0 +1,20 @@
+﻿namespace HTTP
+{
+    internal class HttpClient
+    {
+        private static readonly System.Net.Http.HttpClient client = new()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+        };
+
+        public static async Task<string> Get(string path)
+        {
+            return await client.GetStringAsync(path);
+        }
+
+        public static async Task<HttpResponseMessage> GetResponse(string path)
+        {
+            return await client.GetAsync(path);
+        }
+    }
+}
---
+++ HTTP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Mime;
+
+namespace HTTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string result = HttpClient.Get("todos/").Result;
+            //Console.WriteLine(result);
+
+            HttpResponseMessage response = HttpClient.GetResponse("todos/").Result;
+            HttpContent content = response.Content;
+            HttpHeaders headers = content.Headers;
+            foreach(var header in headers)
+            {
+                Console.WriteLine(header.Key);
+                foreach(var value in header.Value)
+                {
+                    Console.WriteLine($"\t{header.Key}: {value}");
+                }
+            }
+        }
+    }
+}
