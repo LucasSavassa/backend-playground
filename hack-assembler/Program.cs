ece345defe9e29a45575cbf616b05424f156d619
Lucas Savassa <lucaspsavassa@gmail.com>
2025-08-20 12:40:05 -0300

    ./hack-assembler: create basic cli interface

--- hack-assembler/Program.cs
+++ hack-assembler/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace hack_assembler;
+﻿using System.Reflection;
+
+namespace Assembler;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            Console.WriteLine("no command provided. use 'help' for usage information.");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "assemble":
+                HandleArgsAssemble(args);
+                break;
+            case "help":
+                PrintHelp();
+                break;
+            default:
+                Console.WriteLine($"invalid command '{args[0]}'. use 'help' for usage information.");
+                break;
+        }
+    }
+
+    private static void PrintHelp()
+    {
+        Console.WriteLine($"usage: {Assembly.GetExecutingAssembly().GetName().Name} assemble <full-path>");
+        Console.WriteLine("assemble .asm file found in <full-path>");
+    }
+
+    private static void HandleArgsAssemble(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("no file provided. use 'assemble --help' for usage information.");
+            return;
+        }
+
+        switch (args[1])
+        {
+            case "--help":
+                PrintHelp();
+                break;
+            default:
+                HandleAssemble(args[1]);
+                break;
+        }
+    }
+
+    private static void HandleAssemble(string path)
+    {
+        // Here you would add the logic to handle the assembly of the file.
+        Console.WriteLine($"Assembling file: {path}");
     }
 }
