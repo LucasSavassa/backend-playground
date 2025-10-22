fb321a35bf47fd204b57d067a2219c3f5b3d750c
Lucas Savassa <lucaspsavassa@gmail.com>
2025-10-22 10:16:21 -0300

    ./blazor: add game room

---
+++ dotnet/blazor/game/entities/GameRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace entities
+{
+    public class GameRoom
+    {
+        public Guid ID { get; set; }
+        public string Name { get; init; }
+
+        public GameRoom(string name)
+        {
+            ID = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
--- dotnet/blazor/game/game/game/Hubs/GameHub.cs
+++ dotnet/blazor/game/game/game/Hubs/GameHub.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using entities;
+using Microsoft.AspNetCore.SignalR;
 
 namespace game.Hubs
 {
     public class GameHub : Hub
     {
+        private static readonly List<GameRoom> rooms = new();
+
         public override async Task OnConnectedAsync()
         {
             await base.OnConnectedAsync();
 
+            await Clients.Caller.SendAsync("Rooms", rooms.OrderBy(r => r.Name));
+
             Console.WriteLine($"Player with ID '{Context.ConnectionId}' connected.");
+        }
+
+        public async Task<GameRoom> CreateRoom(string playerName, string roomName)
+        {
+            GameRoom room = new(roomName);
+            rooms.Add(room);
+            await Clients.All.SendAsync("Rooms", rooms.OrderBy(r => r.Name));
+            return room;
         }
     }
 }
