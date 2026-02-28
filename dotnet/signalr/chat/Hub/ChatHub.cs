365daac8b27afbb30f56e81d9608b8683df1a274
Lucas Savassa <lucaspsavassa@gmail.com>
2026-02-28 15:35:22 -0300

    ./signalr: add chat

---
+++ dotnet/signalr/chat/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chat
+{
+    public class ChatHub : Hub
+    {
+        public Task Broadcast(string name, string message)
+        {
+            return Clients.All.SendAsync("broadcast", name, message);
+        }
+
+        public Task Echo(string name, string message)
+        {
+            return Clients.Client(Context.ConnectionId).SendAsync("echo", name, message);
+        }
+    }
+}
--- dotnet/signalr/chat/Program.cs
+++ dotnet/signalr/chat/Program.cs
@@ -1,6 +1,14 @@
+using chat;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR().AddAzureSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseRouting();
+app.UseStaticFiles();
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
